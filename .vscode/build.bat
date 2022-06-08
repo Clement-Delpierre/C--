6ae32bd270b222f3f7c59619cd791a89e47fd8ee
Clément Delpierre <clementdelpierre.be@gmail.com>
2022-06-08 19:49:51 +0200

    fait2

--- .vscode/build.bat
+++ .vscode/build.bat
@@ -1,11 +1,26 @@
 @ECHO OFF
 
-SET compil=gcc
-if ".cpp" == "%3" (
-    SET compil=g++
+:: Gestion de l'activation / désactivation des warnings
+Set warnings=-Wall
+IF "%3"=="off" (
+    SET warnings=--no-warnings
+)
+
+:: Gestion de la différence C/C++
+SET build=gcc
+IF "%3"==".cpp" (
+    SET build-g++
+)
+
+SET list=
+>nul find "Modules\point.h" %1 && (
+    SET list= Modules\point.cpp
+)
+>nul find "Modules\vecteur.h" %1 && (
+    SET list=%list% Modules\vecteur.cpp
 )
 
 :: On compile le fichier; si pas d'erreur, on l'exécute
-%compil% -o out\%2.exe %1 && (
+%build% -o out\%2.exe %1%list% %warnings% && (
     out\%2.exe
 )
